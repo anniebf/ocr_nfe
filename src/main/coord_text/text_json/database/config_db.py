b8c616c11806ec0453be27108188a8d0254fd3dc
hianny urt <HIANNY.URT@bomfuturo.com.br>
2025-09-23 16:22:57 -0400

    threads nas funcoes de leitura das coordenadas

--- src/main/coord_text/text_json/database/config_db.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'dns': 'oracle.bomfuturo.local:1521/protheus',
-    'passwordBd': 'Rpa!2023',
-    'usernameBd': 'rpa',
-    'port': 3306
-}
-
-# Chave secreta para tokens
-SECRET_KEY = "supersenhabancooracle"
